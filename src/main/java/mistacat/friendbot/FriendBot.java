c2500b0c0935125dc318a9c395533201bb66705d
MistaCat <secretkitteh750@gmail.com>
2019-03-30 13:37:00 -0400

    New bot

--- src/main/java/mistacat/friendbot/FriendBot.java
+++ src/main/java/mistacat/friendbot/FriendBot.java
@@ -1,8 +1,16 @@
 package mistacat.friendbot;
 
-public class FriendBot {
-    
-    public static void main(String[] args) {
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
 
+import javax.security.auth.login.LoginException;
+
+public class FriendBot {
+    public static JDA client;
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        client = new JDABuilder("token goes here").build();
+
+        client.awaitReady();
     }
 }
